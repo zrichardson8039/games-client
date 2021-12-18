7734ff5d4dcc094da56f31f0f6fbbe1152ffbc1f
zrichardson8039 <zachrichardson1114@gmail.com>
2021-12-18 17:31:51 -0500

    added firebase

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtq0zBTjo_DuaxEXbY93yUbLSu64Buzhc",
+  authDomain: "zachs-ai-games.firebaseapp.com",
+  projectId: "zachs-ai-games",
+  storageBucket: "zachs-ai-games.appspot.com",
+  messagingSenderId: "1026940853604",
+  appId: "1:1026940853604:web:0a105c50d044cc0101ee71",
+  measurementId: "G-N64TM5R7MW",
+}
+
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
